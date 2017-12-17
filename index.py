712b1ef89aba57eb204503426f7934b5193e06ae
Shijit <shijit.dasgupta@gmail.com>
2017-12-17 15:07:35 -0500

    Update to Index

--- index.py
+++ index.py
@@ -1,11 +1,25 @@
 import json
 import datetime
 
-
 def handler(event, context):
+    # Python program to run a Cube Function
+    body = json.loads(event['body'])
+    number_to_cube = body['number_to_cube']
+    # check if the number is negative, positive or zero
+    if number_to_cube < 0:
+        print("Sorry, cube does not exist for negative numbers")
+    elif number_to_cube == 0:
+        print("The cube of 0 is 0")
+    elif number_to_cube > 9000:
+        print("Scouter says its over 9000")
+    else:
+        num_cubed = number_to_cube ** 3
+        print("The cube of ", number_to_cube, "is", num_cubed)
+
     data = {
-        'output': 'Hello World',
+        'output': 'The cube of ' + num + ' is ' + num_cubed ,
         'timestamp': datetime.datetime.utcnow().isoformat()
+
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
