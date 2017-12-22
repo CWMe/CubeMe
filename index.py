ab21cbd45c3d415f69b7d9cada37d23e717bef05
SDasman <shijit.dasgupta@gmail.com>
2017-12-22 14:55:21 -0500

    Update index.py

--- index.py
+++ index.py
@@ -7,11 +7,11 @@ def handler(event, context):
     number_to_cube = int(body['number_to_cube'])
     # check if the number is negative, positive or zero
     if number_to_cube < 0:
-        return respond(ValueError("Sorry, cube does not exist for negative numbers"))
+        return respond("Sorry, cube does not exist for negative numbers")
     elif number_to_cube == 0:
         return respond(None, create_response(0, 0))
     elif number_to_cube > 9000:
-        return respond(ValueError("Scouter says its over 9000. Too High"))
+        return respond("Scouter says its over 9000. Too High")
     else:
         num_cubed = number_to_cube ** 3
         return respond(None, create_response(number_to_cube, num_cubed))
